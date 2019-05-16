deeab551491f6884574e381630b2d875cd8389f1
Bruno Vieira <bruno.vieira@zup.com.br>
2019-05-16 10:54:47 -0300

    Initial Commit

---
+++ continuous_integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  continuous_integration
+//
+//  Created by Bruno Vieira on 16/05/19.
+//  Copyright © 2019 Bruno Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ continuous_integrationTests/continuous_integrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  continuous_integrationTests.swift
+//  continuous_integrationTests
+//
+//  Created by Bruno Vieira on 16/05/19.
+//  Copyright © 2019 Bruno Vieira. All rights reserved.
+//
+
+import XCTest
+@testable import continuous_integration
+
+class continuous_integrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
