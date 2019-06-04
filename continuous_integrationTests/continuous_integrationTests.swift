c0eb15b4db8ab6870ea7892c668cea1ab6caa7c1
Bruno Vieira <bruno.vieira@zup.com.br>
2019-06-04 17:04:05 -0300

    Test succeed

--- continuous_integrationTests/continuous_integrationTests.swift
+++ continuous_integrationTests/continuous_integrationTests.swift
@@ -18,10 +18,17 @@ class continuous_integrationTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
+    
+    func testExample2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
